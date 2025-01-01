a9d925fd916b4ff7a7a81a9c16ac476366bee482
tomoya-kato-lvgs <kato.eng.dao@gmail.com>
2025-01-01 14:52:01 +0900

    first happy new year

---
+++ basic/main.js
@@ -0,0 +1,31 @@
+// const u = '10' - 10;
+// console.log(u);
+// console.log(typeof u);
+
+// console.log('hello' * 10);
+
+// const userInput = '10';
+// let calResult;
+// calcResult = Number(userInput) + 10;
+// calcResult = parseInt(userInput) + 10;
+
+// calcResult = parseFloat(userInput) + 10;
+
+// 何もないことを示す
+// let userInfo; // これはundefinedになる
+// let userUnfo = undefined;
+
+// 何もないことを表すのがJavascriptではundefinedとnullがある
+// undefinedは予期せぬ。
+// nullは予定通り何もないことを表すイメージがある。
+// 一般的には明示的にundefinedを入れることは基本なく、nullにすることが多いとされて入るがプロジェクトによるらしい
+
+// array = [];
+// array.push('apple');
+
+ok = 1 === 1;
+ok = 'hello' === 'hello';
+ok = 1 !== 2;
+// console.log(ok);
+ok = 1 == 1;
+// console.log(ok);
